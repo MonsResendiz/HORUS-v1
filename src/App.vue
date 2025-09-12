61f5dc6b84af7c9ea5df23f187aba0c2d9b4d240
MonsResendiz <monsresendiz.h@gmail.com>
2025-09-12 14:27:54 -0600

    Proyecto Iniciado

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- Menú -->
+    <nav class="flex gap-4 bg-gray-800 p-4 text-white">
+     <!--  <button @click="current = 'Home'" :class="btnClass('Home')">Inicio</button> -->
+
+    </nav>
+
+    <!-- Contenido dinámico -->
+    <component :is="currentComponent" />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import Home from './views/home.vue'
+import About from './views/Dashboard.vue'
+
+const current = ref('Home')
+
+const currentComponent = computed(() => {
+  if (current.value === 'About') return About
+  return Home
+})
+
+const btnClass = (tab) => 
+  current.value === tab 
+    ? 'font-bold underline' 
+    : 'hover:underline'
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="Dashboard">
+     <h1>"Nombre del fiscal"</h1>
+     <h2>
+        <button class="dash" type="button" @click="secciones = !secciones" ></button>
+      </h2>
+      <div v-if="secciones">
+        <!-- Contenido aquí -->
+      </div>     
+   </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      secciones: false
+    }
+  }
+}
+</script>
